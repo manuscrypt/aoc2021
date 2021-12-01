7f6913ae1958d241f013490a3a1e8257a5165c7c
Manuel Baumann <manuel.baumann@noser.com>
2021-12-01 22:10:25 +0100

    go overboard

--- src/Day01.elm
+++ src/Day01.elm
@@ -6,34 +6,93 @@ import Posix.IO.File as File
 import Posix.IO.Process as Proc
 
 
+type Window
+    = Single Int
+    | Triplet ( Int, Int, Int )
+
+
 program : Process -> IO ()
 program _ =
     let
         input =
             readAllLines "C:/src/aoc2021/data/d01.txt"
-                |> toInput
     in
     IO.do
         (IO.combine
             [ sample
-                |> partA 0
+                |> toInputA
+                |> count 0
                 |> output "Sample Part A: "
             , sample
-                |> partB
+                |> toInputB
+                |> count 0
                 |> output "Sample Part B: "
             , input
-                |> IO.map (partA 0)
+                |> IO.map (toInputA >> count 0)
                 |> IO.andThen (output "Part A: ")
             , input
-                |> IO.map partB
+                |> IO.map (toInputB >> count 0)
                 |> IO.andThen (output "Part B: ")
             ]
         )
         return
 
 
-return _ =
-    IO.return ()
+toInputA : List String -> List Window
+toInputA =
+    List.filterMap String.toInt >> List.map Single
+
+
+toInputB : List String -> List Window
+toInputB =
+    List.filterMap String.toInt
+        >> (\nums -> List.zip3 nums (List.drop 1 nums) (List.drop 2 nums))
+        >> List.map Triplet
+
+
+count : Int -> List Window -> Int
+count acc windows =
+    case windows of
+        x :: y :: z ->
+            if sum y > sum x then
+                count (acc + 1) (y :: z)
+
+            else
+                count acc (y :: z)
+
+        _ ->
+            acc
+
+
+sum : Window -> Int
+sum win =
+    case win of
+        Single i ->
+            i
+
+        Triplet ( a, b, c ) ->
+            a + b + c
+
+
+sample : List String
+sample =
+    [ "199"
+    , "200"
+    , "208"
+    , "210"
+    , "200"
+    , "207"
+    , "240"
+    , "269"
+    , "260"
+    , "263"
+    ]
+
+
+output : String -> Int -> IO ()
+output prefix num =
+    IO.do (Proc.print (prefix ++ String.fromInt num))
+        return
 
 
 readAllLines : String -> IO (List String)
@@ -42,66 +101,11 @@ readAllLines filename =
         (File.contentsOf filename
             |> IO.exitOnError identity
         )
-    <|
-        \content ->
-            content
-                |> String.lines
-                |> IO.return
+        (String.lines
+            >> IO.return
+        )
 
 
-toInput : IO (List String) -> IO (List Int)
-toInput lines =
-    lines
-        |> IO.map (List.filterMap String.toInt)
-
-
-partA : Int -> List Int -> Int
-partA acc nums =
-    case nums of
-        x :: y :: z ->
-            if y > x then
-                partA (acc + 1) (y :: z)
-
-            else
-                partA acc (y :: z)
-
-        _ ->
-            acc
-
-
-partB : List Int -> Int
-partB nums =
-    runPartB 0 (List.zip3 nums (List.drop 1 nums) (List.drop 2 nums))
-
-
-runPartB acc windows =
-    case windows of
-        ( a0, a1, a2 ) :: ( b0, b1, b2 ) :: c ->
-            if (b0 + b1 + b2) > (a0 + a1 + a2) then
-                runPartB (acc + 1) (( b0, b1, b2 ) :: c)
-
-            else
-                runPartB acc (( b0, b1, b2 ) :: c)
-
-        _ ->
-            acc
-
-
-output : String -> Int -> IO ()
-output prefix num =
-    IO.do (Proc.print (prefix ++ String.fromInt num)) <| \_ -> IO.return ()
-
-
-sample : List Int
-sample =
-    [ 199
-    , 200
-    , 208
-    , 210
-    , 200
-    , 207
-    , 240
-    , 269
-    , 260
-    , 263
-    ]
+return : a -> IO ()
+return _ =
+    IO.return ()
